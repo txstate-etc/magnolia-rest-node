df7b148c1e52444935de2a125bc7b9268776030e
Christopher Schauer <cschauer@txstate.edu>
2016-11-09 17:44:41 -0600

    refactor

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const Promise = require("bluebird");
-module.exports = {
-  Client: require('./lib/client'),
-  Property: require('./lib/property'),
-  Node: require('./lib/node')
-};
+const Client = require('./lib/client');
+const Node = require('./lib/node');
+const types = require('./lib/node-types');
+
+function Magnolia(options) {
+  this.client = new Client(options);
+}
+
+Magnolia.prototype.node = function(path, type) {
+  return new Node(this.client, path, type);
+}
+
+Magnolia.prototype.page = function(path, template) {
+  return new types.Page(this.client, path, template);
+}
+
+Magnolia.prototype.area = function(path, template) {
+  return new types.Area(this.client, path, template);
+}
+
+Magnolia.prototype.component = function(path, template) {
+  return new types.Component(this.client, path, template);
+}
+
+module.exports = Magnolia;
--- test/lib/client.js
+++ test/lib/client.js
@@ -1,6 +1,4 @@
-const MagnoliaRest = require('../../index');
-const Magnolia = MagnoliaRest.Client;
-const Node = MagnoliaRest.Node;
+const Magnolia = require('../../index');
 const Errors = require('../../lib/errors');
 const should = require('should');
 
@@ -8,6 +6,7 @@ const options = {
   basePath: 'http://localhost:8080/.rest',
   user: 'superuser',
   password: 'superuser',
+  workspace: 'website',
   depth: 1
 };
 
@@ -15,68 +14,79 @@ describe('magnolia-rest', function() {
   this.timeout(5000);
 
   before(function() {
-    this.client = new Magnolia(options);
+    this.magnolia = new Magnolia(options);
   });
 
-  describe('#getNode', function() {
-    it('should return a page', function() {
-      return this.client.getNode('Normal', {depth: 0})
-        .then(console.dir);
+  describe('node', function() {
+    describe('#get', function() {
+      it('should return a node', function () {
+        return this.magnolia.node('/website/Normal').get()
+          .then(node => node.name.should.equal('Normal'));
+      });
+    });
+
+    describe('#create', function() {
+      it('should create a node', function() {
+        return this.magnolia.node('/website/test-node', 'mgnl:page').create()
+          .then(() => this.magnolia.node('/website/test-node').get())
+          .then(node => node.name.should.equal('test-node'));
+      });
+      it('should create nodes recursively', function() {
+        let node = this.magnolia.node('/website/test-recurse', 'mgnl:page');
+        node.addNode(this.magnolia.node('contentParagraph', 'mgnl:area'));
+        return node.create()
+          .then(() => this.magnolia.node('/website/test-recurse/contentParagraph').get())
+          .then(node => node.name.should.equal('contentParagraph'));
+      });
+
+      after(function() {
+        return this.magnolia.node('/website/test-recurse').delete();
+      });
+    });
+
+    describe('#save', function() {
+      it('should update properties', function() {
+        return this.magnolia.node('/website/test-node').get()
+          .then(node => {
+            node.setProperty('test', 'test');
+            return node.save();
+          })
+          .then(() => this.magnolia.node('/website/test-node').get())
+          .then(node => {
+            node.getProperty('test').should.equal('test');
+          });
+      });
+      it('should delete properties', function() {
+        return this.magnolia.node('/website/test-node').get()
+          .then(node => {
+            node.deleteProperty('test');
+            return node.save();
+          })
+          .then(() => this.magnolia.node('/website/test-node').get())
+          .then(node => should(node.getProperty('test')).not.be.ok());
+      });
+    });
+
+    describe('#delete', function() {
+      it('should delete node', function() {
+        return this.magnolia.node('/website/test-node').delete()
+          .then(() => this.magnolia.node('website/test-node').get())
+          .then(() => { throw 'Expected 404' })
+          .catch(e => e.should.be.an.instanceOf(Errors.ResponseError).and.have.property('status', 404));
+      });
     });
   });
 
-  describe('#createPage', function() {
+  describe('page', function() {
     it('should automatically create missing parent pages', function() {
-      return this.client.createPage(Node.newPage('/parents/dont/exist'))
+      let page = this.magnolia.page('/website/parents/dont/exist', 'gato-template-txstate2015:pages/standard-template');
+      return page.create()
+        .then(() => this.magnolia.node('/website/parents/dont/exist').get())
         .then(node => node.name.should.equal('exist'));
     });
-  });
 
-  describe('#save', function() {
-    it('should create a new node', function() {
-      return this.client.save(Node.newPage('/test-page'))
-        .then(node => node.name.should.equal('test-page'));
+    after(function() {
+      return this.magnolia.node('/website/parents').delete();
     });
-    it('should update properties', function() {
-      return this.client.getNode('/test-page')
-        .then(node => {
-          node.setProperty('testProp', 'this is a test');
-          return this.client.save(node);
-        })
-        .then(() => this.client.getNode('/test-page'))
-        .then(node => node.getProperty('testProp').should.equal('this is a test'));
-    });
-    it('should delete properties', function() {
-      return this.client.getNode('/test-page')
-        .then(node => {
-          node.deleteProperty('testProp');
-          return this.client.save(node);
-        })
-        .then(() => this.client.getNode('/test-page'))
-        .then(node => should(node.getProperty('testProp')).not.be.ok());
-    });
-    it('should create nodes recursively', function() {
-      let page = Node.newPage('/create-levels');
-      page.nodes.push(Node.newArea('/create-levels/contentParagraph'));
-      return this.client.save(page)
-        .then(() => this.client.getNode('/create-levels/contentParagraph'))
-        .then(node => node.name.should.equal('contentParagraph'));
-    });
-  });
-
-  describe('#deleteNode', function() {
-    it('should delete node', function() {
-      return this.client.deleteNode('/test-page')
-        .then(() => this.client.getNode('/test-page'))
-        .then(() => { throw 'Expected 404' })
-        .catch(e => {
-          e.should.be.an.instanceOf(Errors.ResponseError).and.have.property('status', 404);
-        });
-    });
-  });
-
-  after(function() {
-    return this.client.deleteNode('/parents')
-      .then(() => this.client.deleteNode('/create-levels'));
   });
 });
